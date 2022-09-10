2aee7446d5e9c657310cdb27acc3d340eaf19e9c
WiktorW-PL <modusii.90@wp.pl>
2022-09-10 14:18:22 +0200

    Created html and styles for login panel

--- components/Login-panel/Inputs.js
+++ components/Login-panel/Inputs.js
@@ -1,10 +1,23 @@
 import './Inputs.css';
+import Button from '../UI/Button.js'
 
 
 const Inputs = () => {
   return (
-    <div className="inputs">
+    <div className="inputs-container">
 
+        <form>
+
+          <label htmlFor="email">Email</label>
+          <input id="email" type="text"/>
+
+          <label htmlFor="password" className="label-password">Password</label>
+          <input id="password" type="password"/>
+
+          <Button type="submit">Log in</Button>
+
+        </form>
+        
     </div>
   );
 }
